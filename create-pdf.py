06b4cbab17e7e0f7a00d38991dbf1c9880aee057
Tomn0 <nal.tomek@gmail.com>
2023-12-01 13:33:13 +0100

    added functionality of multipage pdfs creation, and added text truncation when longer than available width

--- create-pdf.py
+++ create-pdf.py
@@ -9,9 +9,60 @@ def shift_point(point, dx, dy):
     return fitz.Point(new_x, new_y)
 
 
-def encode_to_pdf(text, name, p):
-  lines = text.split("\n")
-  # lines_num = len(lines)
+def truncate_line(line, available_width, font, fontsize):
+    """
+    Truncates a line to fit within the available width without breaking words.
+    """
+    current_width = font.text_length(line, fontsize)
+    if current_width <= available_width:
+        return line  # Line already fits within the available width
+
+    truncated_line = ""
+    # split on whitespace
+    words = line.split()
+
+    for word in words:
+        word_width = font.text_length(word, fontsize)
+
+        if font.text_length(truncated_line + " " + word, fontsize) <= available_width:
+            if truncated_line:
+                truncated_line += " "
+            truncated_line += word
+        else:
+            break  # Stop adding words when the line exceeds the available width
+
+    return truncated_line
+
+def encode_to_pdf(doc, text, name, fontname="helv", fontsize=12, margin=50, max_line_width=100):
+  p = fitz.Point(margin, margin)  # start point of 1st line
+  page = doc.new_page()  # new or existing page via doc[n]
+
+  # Calculate the available height on the page
+  page_height = page.rect.height
+  available_height = page_height - 2 * margin
+
+  page_width = page.rect.width
+  available_width = page_width - 2 * margin
+
+  breakpoint()
+
+  # Split the text into lines based on the available width
+  # lines = []
+  # Split based on newlines from the original text
+  lines = []
+  remaining_text = text.split("\n")
+  
+  for line in remaining_text:
+    # can also be done using font.text_length()
+    line_lenght = fitz.get_text_length(line, fontname=fontname, fontsize=fontsize)
+    if line_lenght >= available_width:
+       # break line
+      font = fitz.Font(fontname)
+      truncated_line = truncate_line(line, available_width, font, fontsize)
+      lines.append(truncated_line)
+    else:
+       lines.append(line)
+
 
   for i, line in enumerate(lines):
     if i % 2 == 0:
@@ -20,53 +71,34 @@ def encode_to_pdf(text, name, p):
       p = shift_point(p, 0, 13)
     page.insert_text(p,
                       line,  # the text (honors '\n')
-                      fontname = "helv",  # the default font
-                      fontsize = 11,  # the default font size
+                      fontname = "tiro",  # the default font
+                      # fontfile = 'fonts\OpenSans_ABCDE-shifted-up-or-down.sfds',
+                      fontsize = fontsize,  # the default font size
                       rotate = 0,  # also available: 90, 180, 270
                       )
+    margin += fontsize * 1.2
+    
+    # # Check if there is enough space for the next line
+    if margin + fontsize * 1.2 > available_height:
+      # Move to the next page
+      page = doc.new_page()
+      
+      # reset text pointer
+      p = fitz.Point(50, 72)
+      # Reset the margin
+      margin = 10
+
 
   doc.save(f"{name}.pdf")
 
 
 if __name__ == '__main__':
   doc = fitz.open()  # new or existing PDF
-  page = doc.new_page()  # new or existing page via doc[n]
-  p = fitz.Point(50, 72)  # start point of 1st line
-  with open("data/anthem.txt", "r", encoding="utf-8") as f:
+
+
+  with open("data/drukus_ad.txt", "r", encoding="utf-8") as f:
     filetxt = "".join(f.readlines())
-    encode_to_pdf(filetxt, "anthem", p)
-
-# text = "Some text,sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssAAAAAAAAAAAAAAAAAAAAAAAsss\nspread across\nseveral lines."
-# text1 = "Some text,"
-# text2 = "spread across"
-# text3 = "several lines."
-# the same result is achievable by
-# text = ["Some text", "spread across", "several lines."]
-
-# page.insert_text(p,  # bottom-left of 1st char
-#                      text,  # the text (honors '\n')
-#                      fontname = "helv",  # the default font
-#                      fontsize = 11,  # the default font size
-#                      rotate = 0,  # also available: 90, 180, 270
-#                      )
-
-# p = fitz.Point(150, 172)
-# page.insert_text(p,  # bottom-left of 1st char
-#                      text2,  # the text (honors '\n')
-#                      fontname = "helv",  # the default font
-#                      fontsize = 11,  # the default font size
-#                      rotate = 0,  # also available: 90, 180, 270
-#                      )
-# p = fitz.Point(300, 272)
-# page.insert_text(p,  # bottom-left of 1st char
-#                      text3,  # the text (honors '\n')
-#                      fontname = "helv",  # the default font
-#                      fontsize = 11,  # the default font size
-#                      rotate = 0,  # also available: 90, 180, 270
-#                      )
-# # print("%i lines printed on page %i." % (rc, page.number))
-
-# doc.save("text.pdf")
+    encode_to_pdf(doc, filetxt, "penal_code_chapter")
 
 
 '''
