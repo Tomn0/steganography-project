86658567861cdd83db24a82d5056d3593d666c33
Tomn0 <nal.tomek@gmail.com>
2023-12-01 13:59:05 +0100

    added TODOs and cleared some comments

--- decode.py
+++ decode.py
@@ -1,6 +1,7 @@
 import fitz  # PyMuPDF
 
 def get_line_spacing(pdf_path, page_number):
+    # TODO iterate over all pages, or take the text as a whole (to check if we are not already doing it by taking the whole block)s
     doc = fitz.open(pdf_path)
     page = doc[page_number]
 
@@ -26,23 +27,10 @@ def get_line_spacing(pdf_path, page_number):
             line_spacing.append(0)
         prev_line = line
             
-    # prev_block = text_blocks[0]
-    # for block in text_blocks:
-    #     lines = block[4].split("\n")  # lines in the block
-    #     if len(lines) > 1:  # Check if there are at least two lines in the block
-    #         for i in range(1, len(lines)):
-    #             prev_line = lines[i - 1]
-    #             current_line = lines[i]
-    #             distance = current_line[1] - prev_line[3]  # Y-coordinate of current line - Bottom Y-coordinate of previous line
-    #             line_spacing.append(distance)
-
     doc.close()
 
     return line_spacing
 
-
-
-# Example usage
 pdf_path = "anthem.pdf"
 page_number = 0  # Replace with the desired page number
 spacing_values = get_line_spacing(pdf_path, page_number)
