98d2a60d92487b71de5be7c5a6cccd4bde9f815a
Tomn0 <nal.tomek@gmail.com>
2024-01-16 13:38:44 +0100

    final working word shift coding

--- word_shift/ws_decode.py
+++ word_shift/ws_decode.py
@@ -3,7 +3,6 @@ import sys
 
 
 def get_word_spacing(pdf_path):
-    # TODO iterate over all pages, or take the text as a whole (to check if we are not already doing it by taking the whole block)s
     doc = fitz.open(pdf_path)
 
     page = doc[0]
@@ -22,7 +21,10 @@ def get_word_spacing(pdf_path):
         for block in text_blocks['blocks']:
             for line in block['lines']:
                 for span in line['spans']:
-                    print(f"Span: {span['text']}")
+                    # print(f"Span: {span['text']}")
+                    # if '\u00A0' in span['text']:
+                    #     print("nbsp found!!!")
+                    #     word_spacing.append(span['size'])
                     if span['text'] == " ":
                         word_spacing.append(span['size'])
                     
@@ -30,17 +32,31 @@ def get_word_spacing(pdf_path):
 
     return word_spacing
 
+def decode(word_spacing):
+    decoded_string = ""
+    zero = min(word_spacing)
+    one = max(word_spacing)
+    for space in word_spacing:
+        if space < one:
+            decoded_string += "0"
+        elif space == one:
+            decoded_string += "1"
+
+    return decoded_string
 
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        pdf_path = "outpud.pdf"
+        pdf_path = "encoded.pdf"
     else:
         pdf_path = sys.argv[1]
 
     spacing_values = get_word_spacing(pdf_path)
 
-    print("spacing values:", spacing_values)
+    # print("spacing values:", spacing_values)
+
+    decoded_str = decode(spacing_values)
+    print(f"Decoded message: {decoded_str}")
 
     # now take only even indexes
     # res_list = spacing_values[1::2]
